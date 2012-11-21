12bc4af602cebce69ab7827ee3dc44b96ba6ed78
mirvin <mark.irvin@versionOne.com>
2012-11-20 19:02:19 -0500

    Threaded the Test Message generator and added progress bar.

--- Generate Messages/TestMsgGen.Designer.cs
+++ Generate Messages/TestMsgGen.Designer.cs
@@ -28,34 +28,36 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
             this.SuspendLayout();
             // 
-            // button1
+            // btnStart
             // 
-            this.button1.Location = new System.Drawing.Point(200, 192);
-            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(100, 28);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Generate";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.btnStart.Location = new System.Drawing.Point(159, 109);
+            this.btnStart.Margin = new System.Windows.Forms.Padding(4);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(100, 28);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Generate";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.button1_Click);
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(319, 107);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox1.Location = new System.Drawing.Point(279, 28);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(132, 22);
+            this.textBox1.Size = new System.Drawing.Size(96, 22);
             this.textBox1.TabIndex = 1;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(52, 111);
+            this.label1.Location = new System.Drawing.Point(44, 32);
             this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(226, 17);
@@ -63,16 +65,31 @@
             this.label1.Text = "How many Messages to generate?";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
-            // Form1
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(106, 157);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(207, 47);
+            this.progressBar1.TabIndex = 3;
+            this.progressBar1.Visible = false;
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker1_ProgressChanged);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_Completed);
+            // 
+            // TestMsgGen
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(508, 348);
+            this.ClientSize = new System.Drawing.Size(418, 248);
+            this.Controls.Add(this.progressBar1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.button1);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.Name = "Form1";
+            this.Controls.Add(this.btnStart);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "TestMsgGen";
             this.Text = "Generate Test Messages";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
@@ -82,9 +99,11 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnStart;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
     }
 }
 
--- Generate Messages/TestMsgGen.cs
+++ Generate Messages/TestMsgGen.cs
@@ -8,14 +8,18 @@ using System.Text;
 using System.Windows.Forms;
 using VersionOne.SDK.ObjectModel;
 using OmMessage = VersionOne.SDK.ObjectModel.Message;
+using System.Threading;
 
 namespace Generate_Messages
 {
     public partial class TestMsgGen : Form
     {
+        private V1Instance v1;
         public TestMsgGen()
         {
             InitializeComponent();
+            backgroundWorker1.WorkerReportsProgress = true;
+            backgroundWorker1.WorkerSupportsCancellation =true;
         }
 
         private void label1_Click(object sender, EventArgs e)
@@ -25,17 +29,39 @@ namespace Generate_Messages
 
         private void button1_Click(object sender, EventArgs e)
         {
-            V1Instance v1 = new V1Instance("https://www11.v1host.com/VersionOneMI/", "admin", "admin");
-            for (int i = 0; i <= Convert.ToInt32(textBox1.Text.ToString());i++)
-            {
-                OmMessage tempMsg = v1.Create.Message("You are a test "+ i.ToString(), "This isa body check", v1.LoggedInMember);
-                tempMsg.Send();
-            }
+            v1 = new V1Instance("https://www11.v1host.com/VersionOneMI/", "admin", "admin");
+            progressBar1.Maximum = Convert.ToInt32(textBox1.Text.ToString());
+            progressBar1.Visible = true;
+            if (backgroundWorker1.IsBusy != true)
+                backgroundWorker1.RunWorkerAsync();
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
+               
+        }
 
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            for (int i = 0; i <= Convert.ToInt32(textBox1.Text.ToString()); i++)
+            {
+                OmMessage tempMsg = v1.Create.Message("You are a test " + i.ToString(), "This is a content", v1.LoggedInMember);
+                tempMsg.Send();
+                backgroundWorker1.ReportProgress(i);
+            }
+
+        }
+        private void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            // Change the value of the ProgressBar to the BackgroundWorker progress.
+            progressBar1.Value = e.ProgressPercentage;
+            // Set the text of this window.
+            this.Text = e.ProgressPercentage.ToString();
+        }
+        private void backgroundWorker1_Completed(object sender, RunWorkerCompletedEventArgs e)
+        {
+            progressBar1.Value = 0;
+            progressBar1.Visible = false;
         }
     }
 }
