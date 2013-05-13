8969d64354fd69e73896609a8275ca36c251b343
mirvin <mark.irvin@versionone.com>
2013-05-13 16:26:30 -0400

    added UI files

---
+++ MessageManagerUI/Session.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using VersionOne.SDK.ObjectModel;
+using MsgManager;
+
+namespace MsgManagerUI
+{
+    public static class Session
+    {
+        public static V1Session V1S;
+    }
+}
