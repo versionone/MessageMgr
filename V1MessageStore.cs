0cd569e142ec4098cc786c576947b59e102e306f
Mark Irvin <MIrvin@corp.versionone.net>
2017-11-30 16:18:52 -0500

    Initial cleaned up Version.  Library only. compiles only.

---
+++ V1MessageStore.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using VersionOne.SDK.APIClient;
+
+namespace MsgManager
+{
+	public class Message
+	{
+		private Oid _message;
+		public Oid message
+		{
+			get { return _message; }
+			set { _message = value; }
+		}
+		private string _messageName;
+		public string messageName
+		{
+			get { return _messageName; }
+			set { _messageName = value; }
+		}
+
+	}
+    public class V1MessageStore
+    {
+//        private MessageFilter messageFilter;
+         
+        //private ICollection<Message> msgList;
+        private V1Connector _v1Instance;
+		public V1Connector v1Instance
+		{
+			get { return _v1Instance; }
+			set { _v1Instance = value; }
+		}
+		private string _userName;
+		public string userName
+		{
+			get { return _userName; }
+			set { _userName = value; }
+		}
+
+
+
+		//This is set up because I have items in StaticTools that should be able to read from the messageContainer for console printing
+		public ICollection<Message> _messageContainer;
+        
+        public ICollection<Message> messageList
+        {
+            get { return _messageContainer; }
+        }
+       
+        public V1MessageStore(V1Connector v1In, string userNameIn)
+        {
+
+            try
+            {
+                v1Instance = v1In;
+				userName = userNameIn;
+				
+ //               messageFilter = new MessageFilter();
+//                messageFilter.Recipient.Add(_v1In.LoggedInMember);
+            }
+            catch(Exception ex)
+            {
+               
+            }
+        }
+       
+        //Load up the store
+        public void QueryMessages()
+        {
+			//   _messageContainer = _v1In.Get.Messages(messageFilter);
+
+			IServices services = new Services(v1Instance);
+			IAssetType storyType = services.Meta.GetAssetType("MessageReceipt");
+			Query query = new Query(storyType);
+
+			IAttributeDefinition nameAttribute = storyType.GetAttributeDefinition("ID");
+			IAttributeDefinition estimateAttribute = storyType.GetAttributeDefinition("Message.Name");
+			query.Selection.Add(nameAttribute);
+			query.Selection.Add(estimateAttribute);
+			QueryResult result = services.Retrieve(query);
+
+			foreach (Asset story in result.Assets)
+			{
+				Console.WriteLine(story.Oid.Token);
+				Console.WriteLine(story.GetAttribute(nameAttribute).Value);
+				Console.WriteLine(story.GetAttribute(estimateAttribute).Value);
+				Console.WriteLine();
+			}
+
+
+		}
+
+        //This returns a copy of the message List to be used by gui or console output
+        //TODO this should Be a simple getter!!!!!!
+        public ICollection<Message> GetMessageList()
+        {
+            return (ICollection<Message>) _messageContainer;
+        }
+
+        public void DeleteSingleMessage(Message msg)
+        {
+            try
+            {
+//                msg.DeleteReceipt();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Problem Attempting to delete a message "+ msg.messageName + " OID="+ msg + " " + e.Message);
+            }
+        }
+
+        public void DeleteAllMessages()
+        {
+  /*          foreach (Message msg in _messageContainer)
+            {
+                DeleteSingleMessage(msg);    
+            }
+        }
+        public Message TraverseList()
+        {
+            return (_messageContainer.ElementAt(1));
+			*/
+        }
+    }
+}
---
+++ V1Session.cs
@@ -0,0 +1,90 @@
+﻿using System;
+//using System.Windows.Forms;
+using VersionOne.SDK.APIClient;
+
+namespace MsgManager
+{
+    public class V1Session
+    {
+        private string _Password;
+        private string _accessToken;
+        private string _UserName;
+        private string _v1Url;
+        private bool isLoggedIn=false;
+        public V1MessageStore messageStore;
+        //public V1Instance V1Inst { get; set; }
+		public V1Connector V1Inst {get; set;}
+
+		public string accessToken
+		{
+			get { return _accessToken; }
+			set { _accessToken = value; }
+		}
+		public string UserName
+        {
+            get { return _UserName; }
+            set { _UserName = value; }
+        }
+
+        public string Password
+        {
+            get { return _Password; }
+            set { _Password = value; }
+        }
+
+        public string Member { get; set; }
+
+
+        public string v1Url
+        {
+            get { return _v1Url; }
+            set { _v1Url = value; }
+        }
+
+        public void SetServerPath(string v1Server)
+        {
+            v1Url = v1Server;
+        }
+
+        public Boolean LoginToV1()
+        {
+            //We will assume that this is true until an exception is thrown to make it false
+            // I did it this way because during debug, I could not determine what values in the V1Inst
+            //That I could use to determine if there was a successful login occurred.  The V1Inst is NOT null when 
+            //it fails and according to the debugger, I cannot access certain threads after the exception has been thrown.
+
+            isLoggedIn = true;
+            try
+            {
+				// if  V1Inst = new V1Instance(_v1URL, _UserName, _Password);
+				//V1Inst.Validate();
+				//V1Inst =  V1Connector(_v1URL)
+				 V1Inst = V1Connector
+					.WithInstanceUrl(v1Url)
+					.WithUserAgentHeader("MessageMgr","1.1")
+					.WithAccessToken(accessToken)
+					.Build();
+            }
+            catch (ArgumentNullException e)
+            {
+                isLoggedIn = false;
+            }
+            catch(Exception)
+            {
+                isLoggedIn = false;
+            }
+            return isLoggedIn;
+        }
+
+        public void LogOutOfV1()
+        {
+            V1Inst = null;
+            isLoggedIn = false;
+        }
+
+        public void PrepGlobalMessage()
+        {
+        //    messageStore = new V1MessageStore(V1Inst);
+        }
+    }
+}
